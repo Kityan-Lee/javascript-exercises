edd7d6a9bae23a8ca93b606f1bdc816b8343ee76
Kityan Lee <irina.kityan.lee@gmail.com>
2022-02-16 20:59:11 +0100

    Finished 05 Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let min = a;
+    let max = b;
 
+    if (typeof a != "number" || typeof b != "number" ||
+        a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    if (a > b) {
+        min = b;
+        max = a;
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
