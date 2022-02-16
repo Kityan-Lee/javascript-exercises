b42130dcc20f8214417347693d0e645eb3fd7528
Kityan Lee <irina.kityan.lee@gmail.com>
2022-02-16 20:50:28 +0100

    Finished 04 Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
+    args.splice(0, 1);
 
+    for (const i of args) {
+        for (const j in array) {
+            if (i == array[j] && typeof i == typeof array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
