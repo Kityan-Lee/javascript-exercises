3e8b9d3320bd6ba1bf17a9a199be825a7f2e0295
Kityan Lee <irina.kityan.lee@gmail.com>
2022-02-17 12:44:04 +0100

    Finished 08 Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (const i of array) {
+    sum += i;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mult = 1;
+	for (const i of array) {
+    mult *= i;
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+	for (let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
